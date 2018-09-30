2834504829ed4d0d2709b5c02349aa1399e60298
avs <avs@omegasoftware.ru>
2018-09-30 21:46:06 +0300

    add time support for log messages

--- src/LogMessage.js
+++ src/LogMessage.js
@@ -8,12 +8,13 @@ import './Highlight.css'
 class LogMessage extends React.Component
 {
   render() {
-    const {lvl, lgr} = this.props.logMessage;
+    const {lvl, lgr, tm} = this.props.logMessage;
 
     return (
       <div className="message_row">
         <ul className="message_row_ul">
-          <li className="log_level" key="log_level">{lvl}</li>
+          <li className="time" key="time">{tm}</li>
+          <li className={"log_level "+lvl}  key="log_level">{lvl}</li>
           <li className="logger" key="logger">{lgr}</li>
         </ul>
         {this.getLogMessage()}
